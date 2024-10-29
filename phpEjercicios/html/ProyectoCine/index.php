9e70c3b647c4652a19e4c9d28b4399c26bf300b1
Adrian140604 <adriansivianes88@gmail.com>
2024-10-29 12:05:17 +0100

    cine

---
+++ phpEjercicios/html/ProyectoCine/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        require_once("database_cine.php");
+        $conexion= Database::getInstance()->getConnection();
+    ?>
+
+    <?php
+        $query="SELECT * FROM pelicula";
+        $stmt=$conexion->query($query);
+        $clientes=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($clientes as $row){
+            echo "Titulo: ".$row["titulo_p"].", Año producción: ".$row["ano_produccion"];
+            echo"<br>";
+        }
+    ?>
+</body>
+</html>
